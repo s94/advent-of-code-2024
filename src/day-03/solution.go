dc74155d04bc5f8c588fbf35184c326a2ece1765
Adam Slater <mr.slater.94@gmail.com>
2024-12-04 16:10:03 +0000

    Day 03 Complete.

---
+++ src/day-03/solution.go
@@ -0,0 +1,65 @@
+package day03
+
+import (
+	"advent-of-code/src/utils"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+var puzzleInput []string = utils.GetPuzzleInput(3)
+
+func MullItOverPartOne() int {
+	var retVal int
+	regex := regexp.MustCompile(`mul\((\d{1,3},\d{1,3})\)`)
+
+	for _, line := range puzzleInput {
+		matches := regex.FindAllStringSubmatch(line, -1)
+		for _, match := range matches {
+			retVal += processMatch(match)
+		}
+	}
+
+	return retVal
+}
+
+func MullItOverPartTwo() int {
+	var retVal int
+	regex := regexp.MustCompile(`mul\((\d{1,3},\d{1,3})\)`)
+	joinedStrings := strings.Join(puzzleInput, "")
+
+	doArray := strings.Split(joinedStrings, "do()")
+	firstString := strings.Split(doArray[0], "don't()")[0]
+
+	firstStringMatches := regex.FindAllStringSubmatch(firstString, -1)
+	for _, match := range firstStringMatches {
+		retVal += processMatch([]string{match[1]})
+	}
+
+	for i, d := range doArray {
+		if i == 0 {
+			continue
+		}
+
+		doString := strings.Split(d, "don't()")[0]
+
+		matches := regex.FindAllStringSubmatch(doString, -1)
+		for _, match := range matches {
+			retVal += processMatch([]string{match[1]})
+		}
+	}
+
+	return retVal
+}
+
+func processMatch(match []string) int {
+	var retVal int
+	for _, m := range match {
+		s := strings.Split(m, ",")
+		x, _ := strconv.Atoi(s[0])
+		y, _ := strconv.Atoi(s[1])
+		retVal += (x * y)
+	}
+
+	return retVal
+}
